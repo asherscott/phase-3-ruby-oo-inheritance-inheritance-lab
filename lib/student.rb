cca2b418317351dcf7de189f4599c9438bc9957b
asher scott <asher.scott6@gmail.com>
2022-04-08 10:34:35 -0500

    commit tree 8162a8bab3bb0d5875a97207609cc36c41e14471

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+
+    def learn stuff
+        @knowledge.push stuff
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
